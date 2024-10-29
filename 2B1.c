e5b0c18f6f6daa7271ef81c4a2e06434a4027a87
Aryan Lokhande <143942666+Aryan-Lokhande@users.noreply.github.com>
2024-10-29 20:25:46 +0530

    Remove unwanted code

--- 2B1.c
+++ 2B1.c
@@ -3,15 +3,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include<stdlib.h>
-void bubble_asc(int arr[],int n)
-{
+void bubble_asc(int arr[],int n){
 	int i,j,temp;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n-i-1;j++)
-		{
-			if(arr[j]>arr[j+1])
-			{
+	for(i=0;i<n;i++){
+		for(j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
 				temp=arr[j];
 
 				arr[j]=arr[j+1];
@@ -20,13 +16,11 @@ void bubble_asc(int arr[],int n)
 		}
 	}
 	printf("\nArray in ascending order: ");
-	for(i=0;i<n;i++)
-	{
+	for(i=0;i<n;i++){
 		printf("\t%d",arr[i]);
 	}
 }
-int main(int argc,char *argv[]) //5 2 7 9 4
-{
+int main(int argc,char *argv[]) {
 	int i,j=2,l,k;
 	int n=argc-2;
 	int a[n];
@@ -36,28 +30,23 @@ int main(int argc,char *argv[]) //5 2 7 9 4
 	{
 		args[i]=argv[j];
 	}
-	args[i]=(char*)0;//0 1 2 3 4 5 6
-	//second 5 2 7 9 4 '0'
-	for(l=0,i=1;i<=n;i++,l++)
-	{
+	args[i]=(char*)0;
+	for(l=0,i=1;i<=n;i++,l++){
 	a[l]=atoi(args[i]);
 	}
 	bubble_asc(a,n);
-	for(l=0,i=1;i<=n;i++,l++)
-	{
+	for(l=0,i=1;i<=n;i++,l++){
 	sprintf(args[i],"%d",a[l]);
 	}
 	pid_t pid;
 	
 	pid=fork();
-	if(pid==0)
-	{
+	if(pid==0){
 	printf("\nI am Child Process,my pid is %d \n",getpid());
 	printf("I am Parent Process,my pid is %d \n",getppid());
 	execve(argv[1],args,NULL);
 	}
-	else
-	{
+	else{
 	k=wait(0);
 	printf("\nI am Parent Process,my pid is %d \n",getpid());
 	printf("I am Parent's Parent Process,my pid is %d\n\n",getppid());
